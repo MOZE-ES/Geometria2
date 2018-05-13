bbb92f3b76e00cc2c78e039920d5eb2c539dd340
Mindaugas Ozeraitis <mindo@hotmail.es>
2018-05-14 00:20:19 +0200

    versión comentada

--- src/Rectangulo_MO.java
+++ src/Rectangulo_MO.java
@@ -1,4 +1,8 @@
-
+/**
+ * 
+ * @author Mindaugas Ozeraitis
+ * @version 1.1
+ */
 public class Rectangulo_MO extends FiguraGeometrica_MO {
 	private double l1;
 	private double l2;
@@ -8,12 +12,16 @@ public class Rectangulo_MO extends FiguraGeometrica_MO {
 		l1 = lG;
 		l2 = lP;
 	}
-
+	/**
+	 *  Este metodo calcula el area del rectangulo
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
+	/**
+	 * Este metodo calcula el perimetro del rectangulo
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
